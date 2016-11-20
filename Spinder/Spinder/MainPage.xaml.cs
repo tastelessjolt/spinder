7a6fd703dcb6ddb70b5f91686c81b57d8d1698de
Harshith G <Harshith G>
2016-11-21 08:44:27 +1100

    testing

--- Spinder/Spinder/MainPage.xaml.cs
+++ Spinder/Spinder/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,12 @@ namespace Spinder
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var msg = new MessageDialog("Content", "Title");
+            await msg.ShowAsync();
         }
     }
 }
