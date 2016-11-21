b9104dd974e8dd8f0c6ef37c7101a726e0aac32e
Harshith G <Harshith G>
2016-11-21 11:30:44 +1100

    Login page created

--- Spinder/Spinder/LoginPage.xaml.cs
+++ Spinder/Spinder/LoginPage.xaml.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,30 +12,26 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Devices.Geolocation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Spinder
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LoginPage : Page
     {
-        public MainPage()
+        public LoginPage()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void button_Click(object sender, RoutedEventArgs e)
         {
-        //    Geolocator geo = new Geolocator();
-        //    Geoposition pos = await geo.GetGeopositionAsync();
-        //    double lat = pos.Coordinate.Point.Position.Latitude;
-        //    double longt = pos.Coordinate.Point.Position.Longitude;
+            String user = usernameBox.Text;
+            String pass = passwordBox.Password;
 
-            this.Frame.Navigate(typeof(Register));
         }
     }
 }
